8a8c4ae7ead453e939bd41b878c8e14272ba1ab0
Shane Madden <shanemadden@users.noreply.github.com>
2025-08-04 17:29:21 -0600

    initial commit with minimal js adaptation from game function

---
+++ README.md
@@ -0,0 +1,5 @@
+# screepsmod-loadwasm
+
+## Fixes the private server loading bots with WebAssembly files
+
+This mod fixes the Screeps private server being unable to load `.wasm` files for bots loaded from local disk or node_modules by the server.
---
+++ index.js
@@ -0,0 +1,37 @@
+const utils = require.main.require('@screeps/backend/lib/utils.js');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = config => {
+  if (config.common) {
+    // Replace the loadBot function in the default backend
+    // https://github.com/screeps/backend-local/blob/1ffc31fc8f2af538c2cdfe92b45299d4a7b86f3e/lib/utils.js#L250
+    // with one compatible with .wasm files
+    utils.loadBot = function(name) {
+      var dir = config.common.bots[name];
+      if(!dir) {
+        throw new Error(`Bot AI with the name "${name}" doesn't exist`);
+      }
+      var stat = fs.statSync(dir);
+      if (!stat.isDirectory()) {
+        throw new Error(`"${dir}" is not a directory`);
+      }
+      fs.statSync(path.resolve(dir, 'main.js'));
+      var files = fs.readdirSync(dir), modules = {};
+      files.forEach(file => {
+        var m = file.match(/^(.*)\.js$/);
+        if(m) {
+          modules[m[1]] = fs.readFileSync(path.resolve(dir, file), {encoding: 'utf8'});
+        } else {
+          var wm = file.match(/^(.*)\.wasm$/);
+          if(wm) {
+            modules[wm[1]] = {
+              binary: fs.readFileSync(path.resolve(dir, file), {encoding: 'base64'}),
+            };
+          }
+        }
+      });
+      return utils.translateModulesToDb(modules);
+    };
+  }
+}
